de5e7d646479378dfe365dbb72a827d3f02e8272
Sasha <zzsanchozz@gmail.com>
2020-10-22 04:25:02 +0300

    fix timer, adding css

--- src/script.js
+++ src/script.js
@@ -1,11 +1,23 @@
-let time = document.getElementById('time');
-let greeting = document.getElementById('greeting');
-let name = document.getElementById('name')
-let goal = document.getElementById('goal');
+let time = document.getElementById("time");
+let greeting = document.getElementById("greeting");
+let name = document.getElementById("name");
+let task = document.getElementById("task");
 let date = new Date();
 let timerId = setInterval(addTime, 60000);
-addTime()
+addTime();
+
+
+name.addEventListener('click', () => name.contentEditable = "true");
+tasks.addEventListener('click', () => tasks.contentEditable = "true");
+
+name.addEventListener("keypress", (e) => {
+    if (e.key == 'Enter') name.contentEditable = "false"
+});
+
+tasks.addEventListener("keypress", (e) => {
+    if (e.key == 'Enter') tasks.contentEditable = "false"
+});
 
 function addTime() {
-    time.innerText = `${date.getHours()}:${date.getMinutes()}`
+    time.innerText = `${new Date().getHours()}:${new Date().getMinutes()}`;
 }
