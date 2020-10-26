d15cb0f7ffbfd63a9966ae0bf997e5bb44c411f3
Sasha <zzsanchozz@gmail.com>
2020-10-26 02:47:07 +0200

    fix timer, fix css

--- src/script.js
+++ src/script.js
@@ -1,22 +1,45 @@
 let time = document.getElementById("time");
-let greeting = document.getElementById("greeting");
 let name = document.getElementById("name");
 let task = document.getElementById("task");
 let timerId = setInterval(addTime, 60000);
-addTime();
 
+addTime()
+localStorageUploader()
+
+function addTime() {
+    let clock = '';
+    if (/^\d\d$/.test(String(new Date().getHours()))) {
+        clock = clock.concat(String(new Date().getHours())).concat(':');
+    } else {
+        clock = clock.concat('0').concat(String(new Date().getHours())).concat(':');
+    }
+    if (/^\d\d$/.test(String(new Date().getMinutes()))) {
+        clock = clock.concat(String(new Date().getMinutes()));
+    } else {
+        clock = clock.concat('0').concat(String(new Date().getMinutes()));
+    }
+    time.innerText = clock;
+}
+
+function localStorageUploader() {
+    if (localStorage.getItem('name')) name.innerText = localStorage.getItem('name');
+    if (localStorage.getItem('task')) task.innerText = localStorage.getItem('task');
+}
 
 name.addEventListener('dblclick', () => name.setAttribute("contentEditable", "true"));
 name.addEventListener("keypress", (e) => {
-    if (e.key == 'Enter') name.setAttribute("contentEditable", "false")
+    if (e.key == 'Enter') {
+        localStorage.setItem('name', name.innerText);
+        name.setAttribute("contentEditable", "false");
+
+    }
 });
 
-tasks.addEventListener('click', () => tasks.setAttribute("contentEditable", "false"));
+task.addEventListener('click', () => task.setAttribute("contentEditable", "true"));
 
-tasks.addEventListener("keypress", (e) => {
-    if (e.key == 'Enter') tasks.setAttribute("contentEditable", "false")
+task.addEventListener("keypress", (e) => {
+    if (e.key == 'Enter') {
+        localStorage.setItem('task', task.innerText);
+        task.setAttribute("contentEditable", "false")
+    }
 });
-
-function addTime() {
-    time.innerText = `${new Date().getHours()}:${new Date().getMinutes()}`;
-}
