2a8bff60b08dbeb8634d9ddfcae5264bba00ba80
Sasha <zzsanchozz@gmail.com>
2020-11-06 15:16:10 +0200

    change name, change clock logic

--- src/script.js
+++ src/script.js
@@ -2,26 +2,22 @@ let time = document.getElementById("time");
 let name = document.getElementById("name");
 let task = document.getElementById("task");
 let divCheckbox = document.getElementById("checkbox");
-let form = document.getElementById("form");
-let text = document.getElementById("text");
+let inputGoal = document.getElementById("inputGoal");
+let InputText = document.getElementById("InputText");
 let divGoal = document.getElementById("goal");
 let check = document.getElementById("check");
 let closeGoal = document.getElementById("closeGoal");
 
-// task.addEventListener("click"), (e) => {
-//     task.sheet.insertRule('#task {text-decoration: line-through}');
-//      task.style.text-decoration = "line-through";
-// }
+task.addEventListener("click", checkCondition);
+check.addEventListener("click", checkCondition);
+name.addEventListener("dblclick", () => name.setAttribute("contentEditable", "true"));
 
-task.addEventListener("click", checkCondition)
-
-
-form.addEventListener("submit", (e) => {
+inputGoal.addEventListener("submit", (e) => {
     e.preventDefault();
     divCheckbox.removeAttribute("hidden");
     divGoal.setAttribute("hidden", "true");
-    task.innerText = text.value;
-    text.value = "";
+    task.innerText = InputText.value;
+    InputText.value = "";
     localStorage.setItem("task", task.innerText);
 
 });
@@ -33,8 +29,6 @@ closeGoal.addEventListener("click", () => {
     localStorage.removeItem("task");
 });
 
-name.addEventListener("dblclick", () => name.setAttribute("contentEditable", "true"));
-
 name.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         localStorage.setItem("name", name.innerText);
@@ -45,33 +39,58 @@ name.addEventListener("keypress", (e) => {
 function checkCondition() {
     if (check.className == "fa fa-square-o") {
         check.className = "fa fa-check-square-o";
-        task.setAttribute = ("style", "text-decoration:line-through");
+        task.style.textDecoration = "line-through";
         localStorage.setItem("check", true);
     } else {
         check.className = "fa fa-square-o";
-        task.setAttribute = ("style", "text-decoration:unlined");
+        task.style.textDecoration = "none";
         localStorage.removeItem("check");
     }
 }
 
-setInterval(function() {
-    //rewrite function into 2 functions
-    let clock = "";
-    if (/^\d\d$/.test(String(new Date().getHours()))) {
-        clock = clock.concat(String(new Date().getHours())).concat(":");
+// setInterval(function() {
+//     let hours = new Date().getHours();
+//     let minutes = new Date().getMinutes();
+//     //rewrite function into 2 functions
+//     let clock = "";
+//     if (/^\d\d$/.test(String(hours))) {
+//         clock = clock.concat(String(hours)).concat(":");
+//     } else {
+//         clock = clock.concat("0").concat(String(hours)).concat(":");
+//     }
+//     if (/^\d\d$/.test(String(minutes))) {
+//         clock = clock.concat(minutes);
+//     } else {
+//         clock = clock.concat("0").concat(String(minutes));
+//     }
+//     time.innerText = clock;
+//     alert('1')
+// }(), 60);
+
+function dateHours() {
+    let hours = new Date().getHours();
+    if (/^\d\d$/.test(String(hours))) {
+        return String(hours).concat(":");
     } else {
-        clock = clock.concat("0").concat(String(new Date().getHours())).concat(":");
+        return ("0").concat(String(hours)).concat(":");
     }
-    if (/^\d\d$/.test(String(new Date().getMinutes()))) {
-        clock = clock.concat(String(new Date().getMinutes()));
+}
+
+function dateMinutes() {
+    let minutes = new Date().getMinutes();
+    if (/^\d\d$/.test(String(minutes))) {
+        return concat(minutes);
     } else {
-        clock = clock.concat("0").concat(String(new Date().getMinutes()));
+        return ("0").concat(String(minutes));
     }
-    time.innerText = clock;
-}(), 60000);
-//let timerId = setInterval(runTime, 60000);
+}
+
+setInterval((function clock() {
+    time.innerText = `${dateHours()}${dateMinutes()}`;
+}), 6000)();
 
 (function localStorageUploader() {
+
     if (localStorage.getItem("name")) {
         name.innerText = localStorage.getItem("name");
     }
@@ -82,5 +101,6 @@ setInterval(function() {
     }
     if (localStorage.getItem("check")) {
         check.className = "fa fa-check-square-o";
+        task.style.textDecoration = "line-through";
     }
 })();
